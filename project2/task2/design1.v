c1a1295d94e9d1cfa317a2c2a6e8adabf5ada501
TRUED0GE <56613357+TRUED0GE@users.noreply.github.com>
2020-07-21 17:13:11 -0700

    Create design1.v

---
+++ project2/task2/design1.v
@@ -0,0 +1,57 @@
+//top circuit
+module p1c1 (in, out, reset, d, e, clk);
+  
+  input in, clk, reset;
+  output reg out;
+  
+  wire a1,b1,c1;
+  output reg d, e;
+  
+  assign a1 = ~(in & d);
+  assign b1 = in ^ e;
+  assign c1 = ~(a1 & b1);
+  assign out = ~(d | e);
+    
+  
+  dff DFF1(.clk(clk),.d(c1),.q(d), .reset(reset));
+  dff DFF2(.clk(clk),.d(b1),.q(e), .reset(reset));
+
+  
+endmodule
+
+//top circuit
+module p1c2 (in, out, reset, d, e, clk);
+  
+  input in, clk, reset;
+  output reg out;
+  
+  wire a2,b2,c2;
+  output reg d, e;
+  
+  assign a2 = ~(in & d);
+  assign b2 = in ^ e;
+  assign c2 = ~(a2 & b2);
+  assign out = ~(d | e);
+    
+  
+  dff DFF1(.clk(clk),.d(c2),.q(d), .reset(reset));
+  dff DFF2(.clk(clk),.d(b2),.q(e), .reset(reset));
+
+  
+endmodule
+
+// Design
+// D flip-flop
+module dff (clk,d,q, reset);
+  input      clk, d, reset;
+  output reg q;
+
+  always @(posedge clk or posedge reset)
+    if (reset) begin
+      q <= 0;
+    end else begin
+      q <= d;
+    end
+  
+  
+endmodule
